3369efd71c74c096611844c1292c919986992f40
sonamk15 <sonamk18@navgurukul.org>
2019-07-15 14:27:47 +0500

    work in prograssing

--- helloword.js
+++ helloword.js
@@ -1,12 +1,9 @@
-Js questions
-1. "https://www.hackerrank.com/challenges/js10-hello-world/problem"
 
-solution==>
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
 *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
 **/
-
+var parameterVariable = "Welcome to 10 Days of JavaScript!"
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
